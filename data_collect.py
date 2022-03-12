ada3b72e130393971f38b6c0c39acb48265017bb
DianiboyPy <jan_mohamed@hotmail.com>
2022-03-12 16:28:15 +0700

    Changes  made by Jan 0n 12/03

--- data_collect.py
+++ data_collect.py
@@ -3,22 +3,25 @@ import json
 from io import BytesIO
 from PIL import Image
 
+airvisualapi_key = "your_airvisual_api_key"
+openweatherapi_key = "your_openweather_api_key"
+
+
 def get_data():
-#Get latitude and longitude of nearest city from Air Visual
-    url = "http://api.airvisual.com/v2/nearest_city?key=your_AirVisula_API_key"
+    # Get latitude and longitude of nearest city from Air Visual
+    url = "http://api.airvisual.com/v2/nearest_city?key=" + airvisualapi_key
     response = requests.request('GET', url)
     raw_data = json.loads(response.text)
     measurements = []
-    #print(raw_data)
+    # print(raw_data)
     lon = raw_data["data"]["location"]["coordinates"][0]
     lat = raw_data["data"]["location"]["coordinates"][1]
     print("Latitude is ", lat, "Longitude is ", lon)
     # Get weather from OpenWeather using latitude and longitude from Air Visua;
     exclude = "current,minutely,daily"
-    api_key = "Your Openweather API key"
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude={}&appid={}".format(lat,lon,exclude, api_key)
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude={}&appid={}".format(
+        lat, lon, exclude, openweatherapi_key)
     response = requests.get(url)
     data = json.loads(response.text)
-   # print(data)
+    print(data)
     return data
-   
--- display_conf.py
+++ display_conf.py
@@ -5,50 +5,61 @@ from io import BytesIO
 import datetime
 
 
-display = pygame.display.set_mode((480,256))
+display = pygame.display.set_mode((480, 256))
 
-        # Display the information
+# Display the information
+
+
 def data2_display(data, hour_req):
-    
+
     pygame.init()
     font = pygame.font.SysFont("arial", 24)
     h1_font = pygame.font.SysFont("arial", 32)
     #display = pygame.display.set_mode((480,256))
 # Fill the display with this colour
-    display.fill((100,10,10))
-    pygame.display.set_caption("Weather")  
+    display.fill((100, 10, 10))
+    pygame.display.set_caption("Weather")
     pygame.display.flip()
-    #pygame.display.update()  
-
+    # pygame.display.update()
 
     current_icon = (data["hourly"][hour_req]['weather'][0]["icon"])
 
-        # create image from URL
-    rsp = requests.get('http://openweathermap.org/img/wn/'+current_icon+'@4x.png')
+    # create image from URL
+    rsp = requests.get('http://openweathermap.org/img/wn/' +
+                       current_icon+'@4x.png')
     pilimage = Image.open(BytesIO(rsp.content)).convert("RGBA")
 
-    pgimg = pygame.image.fromstring(pilimage.tobytes(), pilimage.size, pilimage.mode)
+    pgimg = pygame.image.fromstring(
+        pilimage.tobytes(), pilimage.size, pilimage.mode)
 
-    display.blit(pgimg,(30,40))
+    display.blit(pgimg, (30, 40))
     #current_weather = h1_font.render('Current weather', True, (255,255,255))
     date_info = data["hourly"][hour_req]["dt"]
-    current_weather = font.render((datetime.datetime.fromtimestamp(data["hourly"][hour_req]["dt"]).strftime('%m-%d %H:%M')), True, (255,255,255))
-    display.blit(current_weather,(10,30))
-    current_temp = font.render('Temperature: '+f'{(data["hourly"][hour_req]["temp"])-273:.2f} C', True, (255,255,255))
-    display.blit(current_temp,((display.get_width()-current_temp.get_width())-20,90))
-    current_hum = font.render('Humidity: '+str((data["hourly"][hour_req]["humidity"]))+'%', True, (255,255,255))
-    display.blit(current_hum,((display.get_width()-current_hum.get_width())-20,120))
-    current_wind = font.render('Wind speed: '+f'{(((data["hourly"][hour_req]["wind_speed"])*3600)/1000):.0f}  kph', True, (255,255,255))
-    display.blit(current_wind,((display.get_width()-current_wind.get_width())-20,150))
-    display.blit(current_weather,(10,30))
+    current_weather = font.render((datetime.datetime.fromtimestamp(
+        data["hourly"][hour_req]["dt"]).strftime('%m-%d %H:%M')), True, (255, 255, 255))
+    display.blit(current_weather, (10, 30))
+    current_temp = font.render(
+        'Temperature: '+f'{(data["hourly"][hour_req]["temp"])-273:.2f} C', True, (255, 255, 255))
+    display.blit(
+        current_temp, ((display.get_width()-current_temp.get_width())-20, 90))
+    current_hum = font.render(
+        'Humidity: '+str((data["hourly"][hour_req]["humidity"]))+'%', True, (255, 255, 255))
+    display.blit(
+        current_hum, ((display.get_width()-current_hum.get_width())-20, 120))
+    current_wind = font.render(
+        'Wind speed: '+f'{(((data["hourly"][hour_req]["wind_speed"])*3600)/1000):.0f}  kph', True, (255, 255, 255))
+    display.blit(
+        current_wind, ((display.get_width()-current_wind.get_width())-20, 150))
+    display.blit(current_weather, (10, 30))
 # Final print to display
-    
-    pygame.display.update() 
+
+    pygame.display.update()
     print("Display done")
 
-    #Remove thus at a alter date 
+    # Remove thus at a alter date
     while True:
-           
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit(); exit()
+                pygame.quit()
+                exit()
