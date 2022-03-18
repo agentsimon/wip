843ecc7cd095cf237cc24fc65f8e7c2d56925f90
DianiboyPy <jan_mohamed@hotmail.com>
2022-03-18 19:51:29 +0700

    Changes made by JAN

--- service.py
+++ service.py
@@ -1,11 +1,13 @@
-import time
+# import time
 import data_collect
 import display_conf
+import main_conductor
 
 
 if __name__ == '__main__':
     # service.py executed as script
     # do something
 
-    data = data_collect.get_data()
-    display_conf.data2_display(data, hour_req=0)
+    # data = data_collect.get_data()
+    # display_conf.data2_display(data, hour_req=0)
+    main_conductor.start_conducting()
